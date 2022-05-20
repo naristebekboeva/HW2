1dcf2cfecc681de911956e2ff27675fdfea0d1d7
Nariste Bekboeva <nariste.bekboeva.kk@gmail.com>
2022-05-20 23:21:06 +0600

    add file

---
+++ JS/script1.js
@@ -0,0 +1,5 @@
+// **Описание задания**1. Дана строка **'ddd@bbb@ccc'**. Замените все **@** на **'!'**
+   
+   /* let text = "ddd@bbb@ccc";
+    let newText = text.replaceAll("@", "!");
+    console.log(newText);*/
